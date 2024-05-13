41d4d3972b6ba9512e7135d4fd5a2f84a42ff91a
Anthony Tleiji <anthonytleiji@gmail.com>
2024-05-14 00:35:44 +0300

    Setup shadcn-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`font-sans ${inter.variable}`}>
+        <body className={`font-sans ${inter.variable} dark`}>
           <div className="grid h-screen grid-rows-[auto,1fr]">
             <NextSSRPlugin
               /**
