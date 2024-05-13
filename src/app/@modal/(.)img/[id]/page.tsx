2c5a10933c50ee290fd906840349c3752f0a41ab
Anthony Tleiji <anthonytleiji@gmail.com>
2024-05-13 17:33:08 +0300

    Setup Parallel Route for images Fixed dialog full screen Add getImage query Add component for image view

---
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
+
+export default async function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+  const idAsNumber = Number(photoId);
+  if (isNaN(idAsNumber)) {
+    throw new Error("Invalid photo ID");
+  }
+
+  return (
+    <Modal>
+      <FullPageImageView id={idAsNumber} />
+    </Modal>
+  );
+}
---
+++ src/app/img/[id]/page.tsx
@@ -0,0 +1,14 @@
+import FullPageImageView from "~/components/full-image-page";
+
+export default async function PhotoPage({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+  const idAsNumber = Number(photoId);
+  if (isNaN(idAsNumber)) {
+    throw new Error("Invalid photo ID");
+  }
+
+  return <FullPageImageView id={idAsNumber} />;
+}
